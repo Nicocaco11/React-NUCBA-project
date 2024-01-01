5b984c34e76b737abbf5e449d4cd19b3edc6b615
Nicocaco11 <nicogabrielvidela11@gmail.com>
2024-01-01 16:42:26 -0300

    Inicio del proyecto

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav id="nav">
+      <div className="nav__content">
+        <a href="#">
+          <img src="" alt="" />
+        </a>
+        <a href="#">Inicio</a>
+        <a href="#">Productos</a>
+        <a href="#">Nòsotros</a>
+        <a href="#">Carrito</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
